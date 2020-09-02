430997a5436055d9f19215d8f1b25a3858fd9656
Rorazoro <rorazoro@gmail.com>
2020-09-02 16:04:37 -0400

    ci script changes

--- CI/archive.sh
+++ CI/archive.sh
@@ -3,8 +3,9 @@ set -x
 
 echo "Archiving Builds"
 
-cd ..
 mkdir -p "$ARTIFACTS"
 cd Builds
 
 for i in */; do zip -r "${i%/}.zip" "$i"; mv "${i%/}.zip" "$ARTIFACTS"; done
+
+cd ..
--- CI/localarchive.sh
+++ CI/localarchive.sh
@@ -1,7 +1,7 @@
 set -e
 set -x
 
-export PROJECT_PATH="/mnt/e/My Code/PersonalProjects/Unity/Unity3D-Jenkins"
-export ARTIFACTS="$PROJECT_PATH/_artifacts"
+export PROJECT_PATH=/mnt/e/My Code/PersonalProjects/Unity/Unity3D-Jenkins
+export ARTIFACTS=$PROJECT_PATH/_artifacts
 
-bash ./archive.sh
+bash ./CI/archive.sh
