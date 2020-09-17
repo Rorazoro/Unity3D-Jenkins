ce9a60dbbc292f6278b2be43ea79834e41931189
Rorazoro <rorazoro@gmail.com>
2020-09-17 17:00:00 -0400

    jenkins fix

--- CI/docker_build.ps1
+++ CI/docker_build.ps1
@@ -1,10 +1,2 @@
 
-docker run -e BUILD_NAME \
-  -e UNITY_LICENSE_CONTENT \
-  -e BUILD_TARGET \
-  -e UNITY_USERNAME \
-  -e UNITY_PASSWORD \
-  -w /project/ \
-  -v $(pwd):/project/ \
-  $IMAGE_NAME \
-  /bin/bash -c "/project/ci/before_script.sh && /project/ci/build.sh"
+docker run -e BUILD_NAME -e UNITY_LICENSE_CONTENT -e BUILD_TARGET -e UNITY_USERNAME -e UNITY_PASSWORD -w /project/ -v $(pwd):/project/ $IMAGE_NAME /bin/bash -c "/project/ci/before_script.sh && /project/ci/build.sh"
